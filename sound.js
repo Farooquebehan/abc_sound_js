59890b5d91795ceb04d4fceab080be03d14fe54a
Farooq Behan <56262805+Farooquebehan@users.noreply.github.com>
2023-09-03 03:42:41 -0700

    Add files via upload

---
+++ sound.js
@@ -0,0 +1,155 @@
+function A(){
+
+    document.getElementById('A').play();
+    
+  }
+
+  function B(){
+
+    document.getElementById('B').play();
+    
+  }
+
+  function C(){
+
+    document.getElementById('C').play();
+    
+  }
+
+  function D(){
+
+    document.getElementById('D').play();
+    
+  }
+
+  function E(){
+
+    document.getElementById('E').play();
+    
+  }
+
+  function F(){
+
+    document.getElementById('F').play();
+    
+  }
+
+  function G(){
+
+    document.getElementById('G').play();
+    
+  }
+
+
+  function H(){
+
+    document.getElementById('H').play();
+    
+  }
+  function I(){
+
+    document.getElementById('I').play();
+    
+  }
+  function J(){
+
+    document.getElementById('J').play();
+    
+
+  }
+  function K(){
+
+    document.getElementById('K').play();
+    
+  }
+  function L(){
+
+    document.getElementById('L').play();
+    
+  }
+  function M(){
+
+    document.getElementById('M').play();
+    
+  }
+  function N(){
+
+    document.getElementById('N').play();
+    
+  }
+  function O(){
+
+    document.getElementById('o').play();
+    
+  }
+  function P(){
+
+    document.getElementById('P').play();
+    
+  }
+  function Q(){
+
+    document.getElementById('Q').play();
+    
+  }
+  function R(){
+
+    document.getElementById('R').play();
+    
+  }
+
+  function S(){
+
+    document.getElementById('S').play();
+    
+  }
+  function T(){
+
+    document.getElementById('T').play();
+    
+  }
+  function U(){
+
+    document.getElementById('U').play();
+    
+  }
+  function V(){
+
+    document.getElementById('V').play();
+    0
+  }
+  function W(){
+
+    document.getElementById('W').play();
+    
+  }
+  function X(){
+
+    document.getElementById('X').play();
+    
+  }
+  function Y(){
+
+    document.getElementById('Y').play();
+    
+  }
+  function Z(){
+
+    document.getElementById('Z').play();
+    
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
